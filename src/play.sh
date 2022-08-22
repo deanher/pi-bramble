96cec0bd26ce5b31429126a098dc87b8d676947a
Dean Herringer <deanher@gmail.com>
2022-08-22 19:28:37 +0200

    cleanup

--- src/play.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/ansible-playbook
-
-ansible-playbook -i inventory prepare-ansible-controller.yml --ask-vault-pass
-ansible-playbook -i inventory prepare-hat-cluster.yml --ask-vault-pass
-ansible-playbook -i inventory setup-k3s-cluster.yml --ask-vault-pass
